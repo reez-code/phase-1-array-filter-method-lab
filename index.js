9f9584b29a8f022795c7156f8c9219817998e3c2
Phil <phil.github@gmail.com>
2024-10-15 15:16:01 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string) {
+  const drivers = array.filter((element) => {
+    return element.toLowerCase() === string.toLowerCase();
+  });
+  return drivers;
+}
+
+function fuzzyMatch(array, string) {
+  const drivers = array.filter((element) => {
+    return element.startsWith(string);
+  });
+  return drivers;
+}
+
+function matchName(array, string) {
+  const drivers = array.filter((element) => {
+    return element["name"] === string;
+  });
+  return drivers;
+}
